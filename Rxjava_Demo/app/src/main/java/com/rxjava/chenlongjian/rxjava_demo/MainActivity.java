77a2085a818c894531252f2a6559a86a05ea22da
cslong <504768527@qq.com>
2016-07-05 22:34:41 +0800

    Rxjava 练习样例

--- Rxjava_Demo/app/src/main/java/com/rxjava/chenlongjian/rxjava_demo/MainActivity.java
+++ Rxjava_Demo/app/src/main/java/com/rxjava/chenlongjian/rxjava_demo/MainActivity.java
@@ -1,13 +1,141 @@
 package com.rxjava.chenlongjian.rxjava_demo;
 
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+
+    private LogAdapter _adapter;
+    private List<String> _logs;
+    private  ListView _logsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        _logsList = (ListView)findViewById(R.id.log_list);
+
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createObserver()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.w("Rxjava","onCompleted");
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.w("Rxjava","onError:" + e.getMessage());
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.w("Rxjava","onNext:" + integer);
+                        _log("onNext"+integer);
+
+                    }
+                });
+            }
+        });
+
+        _setupLogger();
     }
+
+
+    private Observable<Integer> createObserver() {
+        return Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                if (!subscriber.isUnsubscribed()) {
+                    for (int i = 0; i < 5; i++) {
+                        int temp = new Random().nextInt(10);
+                        if (temp > 8) {
+                            //if value>8, we make an error
+                            subscriber.onError(new Throwable("value >8"));
+                            break;
+                        } else {
+                            subscriber.onNext(temp);
+                            _log("call"+temp);
+                        }
+                        // on error,complete the job
+                        if (i == 4) {
+                            subscriber.onCompleted();
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+
+    private void _log(String logMsg) {
+
+        if (_isCurrentlyOnMainThread()) {
+            _logs.add(0, logMsg + " (main thread) ");
+            _adapter.clear();
+            _adapter.addAll(_logs);
+        } else {
+            _logs.add(0, logMsg + " (NOT main thread) ");
+
+            // You can only do below stuff on main thread.
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+
+                @Override
+                public void run() {
+                    _adapter.clear();
+                    _adapter.addAll(_logs);
+                }
+            });
+        }
+    }
+
+
+    private void _setupLogger() {
+        _logs = new ArrayList<String>();
+        _adapter = new LogAdapter(this, new ArrayList<String>());
+        _logsList.setAdapter(_adapter);
+    }
+
+    private boolean _isCurrentlyOnMainThread() {
+        return Looper.myLooper() == Looper.getMainLooper();
+    }
+
+
+    private class LogAdapter
+            extends ArrayAdapter<String> {
+
+        public LogAdapter(Context context, List<String> logs) {
+            super(context, R.layout.item_log, R.id.item_log, logs);
+        }
+    }
+
 }
