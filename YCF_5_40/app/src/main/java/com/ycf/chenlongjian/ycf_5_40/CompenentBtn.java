86dd02f88fb1ac3d3c56959595db36d852d66f32
cslongjian <504768527@qq.com>
2016-10-08 17:59:30 +0800

    组合控件

--- YCF_5_40/app/src/main/java/com/ycf/chenlongjian/ycf_5_40/CompenentBtn.java
+++ YCF_5_40/app/src/main/java/com/ycf/chenlongjian/ycf_5_40/CompenentBtn.java
@@ -3,6 +3,7 @@ package com.ycf.chenlongjian.ycf_5_40;
 import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -31,31 +32,30 @@ public class CompenentBtn extends LinearLayout{
         setOrientation(VERTICAL);
         LayoutInflater.from(context).inflate(R.layout.compenentbtn, this);
         btn = (Button) findViewById(R.id.btn);
-        mianyuyue = (Button) findViewById(R.id.mianyuyue);
-//        titleText = (TextView) findViewById(R.id.title_text);
-//        leftButton = (Button) findViewById(R.id.button_left);
-//        leftButton.setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                ((Activity) getContext()).finish();
-//            }
-//        });
+        mianyuyue = (Button) findViewById(R.id.mianyuyuebtn);
         setOnTouchListener(onTouch);
     }
 
-    public void SetType(int type)
+    public void setType(int type)
     {
         current_type = type;
         switch (type)
         {
             case TYPE_NORMAL:
-
                 break;
             case TYPE_MIANYUYUE:
+                mianyuyue.setVisibility(View.VISIBLE);
                 break;
             case TYPE_PRESALE:
+                btn.setBackground(getContext().getResources().getDrawable(R.drawable.detail_list_btn_presale_bg));
+//                btn.setTextColor(getContext().getResources().getColor(R.color.cg_50));
                 break;
             case TYPE_GROUPPRESALE:
+//                btn.setBackground(R.drawable.detail_list_description_btn_top_bg_presale);
+                btn.setBackgroundResource(R.drawable.detail_list_description_btn_top_bg_presale);
+                mianyuyue.setVisibility(View.VISIBLE);
+                mianyuyue.setBackgroundResource(R.drawable.detail_list_description_btn_bottom_bg_presale);
+                mianyuyue.setTextColor(getContext().getResources().getColor(R.color.cg_50));
                 break;
             default:
                 break;
@@ -72,21 +72,74 @@ public class CompenentBtn extends LinearLayout{
     {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
-            switch (current_type)
-            {
-                case TYPE_NORMAL:
 
-                    break;
-                case TYPE_MIANYUYUE:
-                    break;
-                case TYPE_PRESALE:
-                    break;
-                case TYPE_GROUPPRESALE:
-                    break;
-                default:
-                    break;
+            int action =motionEvent.getAction();
 
+            if(action==MotionEvent.ACTION_DOWN){
+                System.out.println("ACTION_DOWN");
+                Log.w("MotionEvent","ACTION_DOWN");
+                switch (current_type)
+                {
+                    case TYPE_NORMAL:
+                        btn.setBackground(getContext().getResources().getDrawable(R.drawable.detail_list_btn_presale_bg));
+                        break;
+                    case TYPE_MIANYUYUE:
+                        break;
+                    case TYPE_PRESALE:
+                        break;
+                    case TYPE_GROUPPRESALE:
+                        btn.setBackgroundResource(R.drawable.detail_list_description_btn_top_bg_press_presale);
+                        mianyuyue.setBackgroundResource(R.drawable.detail_list_description_btn_bottom_bg_press_presale);
+                        break;
+                    default:
+                        break;
+                }
             }
+
+            if(action==MotionEvent.ACTION_MOVE){
+                System.out.println("ACTION_MOVE");
+                Log.w("MotionEvent","ACTION_MOVE");
+                switch (current_type)
+                {
+                    case TYPE_NORMAL:
+                        btn.setBackground(getContext().getResources().getDrawable(R.drawable.detail_list_btn_presale_bg));
+                        break;
+                    case TYPE_MIANYUYUE:
+                        break;
+                    case TYPE_PRESALE:
+                        break;
+                    case TYPE_GROUPPRESALE:
+                        btn.setBackgroundResource(R.drawable.detail_list_description_btn_top_bg_press_presale);
+                        mianyuyue.setBackgroundResource(R.drawable.detail_list_description_btn_bottom_bg_press_presale);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if(action==MotionEvent.ACTION_UP){
+                System.out.println("ACTION_UP");
+                Log.w("MotionEvent","ACTION_UP");
+                switch (current_type)
+                {
+                    case TYPE_NORMAL:
+                        btn.setBackground(getContext().getResources().getDrawable(R.drawable.detail_list_btn_bg));
+                        break;
+                    case TYPE_MIANYUYUE:
+                        break;
+                    case TYPE_PRESALE:
+                        break;
+                    case TYPE_GROUPPRESALE:
+                        btn.setBackgroundResource(R.drawable.detail_list_description_btn_top_bg_presale);
+                        mianyuyue.setBackgroundResource(R.drawable.detail_list_description_btn_bottom_bg_presale);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
+
             return false;
         }
     };
--- YCF_5_40/app/src/main/java/com/ycf/chenlongjian/ycf_5_40/MainActivity.java
+++ YCF_5_40/app/src/main/java/com/ycf/chenlongjian/ycf_5_40/MainActivity.java
@@ -2,12 +2,58 @@ package com.ycf.chenlongjian.ycf_5_40;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CompenentBtn normal;
+    private CompenentBtn mianyuyue;
+    private CompenentBtn presale;
+    private CompenentBtn grouppresale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        normal = (CompenentBtn) findViewById(R.id.normal);
+        normal.setType(CompenentBtn.TYPE_NORMAL);
+
+        normal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.w("MotionEvent","MotionEventMotionEventMotionEventMotionEventMotionEvent");
+            }
+        });
+
+        mianyuyue = (CompenentBtn) findViewById(R.id.mianyuyue);
+        mianyuyue.setType(CompenentBtn.TYPE_MIANYUYUE);
+        mianyuyue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.w("MotionEvent","MotionEventMotionEventMotionEventMotionEventMotionEvent");
+            }
+        });
+
+        presale = (CompenentBtn) findViewById(R.id.presale);
+        presale.setType(CompenentBtn.TYPE_PRESALE);
+        presale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.w("MotionEvent","MotionEventMotionEventMotionEventMotionEventMotionEvent");
+            }
+        });
+
+        grouppresale = (CompenentBtn) findViewById(R.id.grouppresale);
+        grouppresale.setType(CompenentBtn.TYPE_GROUPPRESALE);
+        grouppresale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.w("MotionEvent","MotionEventMotionEventMotionEventMotionEventMotionEvent");
+            }
+        });
+
+
     }
 }
